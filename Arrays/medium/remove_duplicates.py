adf48b5d0ff6be8a430b221d2bc56a414151d0af
ash-gitty <ashrithaneelanjanas@gmail.com>
2025-10-22 19:53:46 +0530

    removing duplicates with two methods

--- Arrays/medium/remove_duplicates.py
+++ Arrays/medium/remove_duplicates.py
@@ -5,3 +5,15 @@ for i in range(len(a)):
     if i==0 or a[i]!=a[i-1]:
         unique.append(a[i])
 print(unique)
+
+#using two pointer method
+
+a=[1,2,3,4,4,5,6,7,8]
+i=0
+
+for j in range(1,len(a)):
+    if a[j]!=a[i]:
+        i+=1
+        a[i]=a[j]
+print(a[:i+1])
+print(i+1)
