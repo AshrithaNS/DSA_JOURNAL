6cf316e931e8b759ec64d3b0f3e7b9260dbce6a9
ash-gitty <ashrithaneelanjanas@gmail.com>
2025-10-29 07:27:18 +0530

    rotating an array by k positions

---
+++ Arrays/medium/rotate_array_by_k_positions.py
@@ -0,0 +1,8 @@
+#rotating array by k positions
+a = [ 13,4,5,56,65,23]
+k=2
+n=len(a)
+k = k%n
+a.reverse()      #whole array is reversed
+a[:k] = reversed(a[:k])   # k number of elements are reversed
+a[k:] = reversed(a[k:])   # all elements got reversed except k elements
